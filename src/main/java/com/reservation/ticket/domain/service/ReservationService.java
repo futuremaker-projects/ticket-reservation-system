6492d68affc3f57f09c8ac44303d2ebe16c8659d
futuremaker <futuremaker019@gmail.com>
2024-07-09 00:57:40 +0900

    `ReservationService` 생성

---
+++ src/main/java/com/reservation/ticket/domain/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.reservation.ticket.domain.service;
+
+import com.reservation.ticket.domain.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+
+
+}
---
+++ src/test/java/com/reservation/ticket/domain/service/ReservationServiceTest.java
@@ -0,0 +1,35 @@
+package com.reservation.ticket.domain.service;
+
+import com.reservation.ticket.domain.repository.ReservationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationServiceTest {
+
+    @InjectMocks ReservationService sut;
+    @Mock ReservationRepository reservationRepository;
+
+    /**
+     * 예약을 진행한다. 예약시 콘서트 스케줄 id, 자리 id 필요
+     */
+    @Test
+    void given_when_then() {
+        // given
+        Long concertScheduleId = 1L;
+        List<Integer> seats = List.of(1, 2);
+
+
+        // when
+
+        // then
+    }
+
+}
