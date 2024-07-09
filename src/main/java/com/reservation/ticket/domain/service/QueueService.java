9508f5ccbf5f512ec3b93ccb232a03bfdc7d704c
futuremaker <futuremaker019@gmail.com>
2024-07-10 01:11:36 +0900

    대기열 테스트 및 구현

--- src/main/java/com/reservation/ticket/domain/service/QueueService.java
+++ src/main/java/com/reservation/ticket/domain/service/QueueService.java
@@ -1,13 +1,54 @@
 package com.reservation.ticket.domain.service;
 
+import com.reservation.ticket.domain.command.QueueCommand;
+import com.reservation.ticket.domain.entity.Queue;
+import com.reservation.ticket.domain.entity.UserAccount;
+import com.reservation.ticket.domain.enums.QueueStatus;
 import com.reservation.ticket.domain.repository.QueueRepository;
+import com.reservation.ticket.domain.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class QueueService {
 
     private final QueueRepository queueRepository;
+    private final UserAccountRepository userAccountRepository;
 
+    @Transactional
+    public QueueCommand.Get createQueue(Long userId) {
+        UserAccount userAccount = userAccountRepository.findById(userId);
+
+        int countActiveStatus = queueRepository.countByStatus(QueueStatus.ACTIVE);
+
+        Queue queue = Queue.of();
+        queue.saveStatusInQueue(countActiveStatus);
+        queue.saveData(userAccount, generateToken());
+
+        return QueueCommand.Get.from(queueRepository.save(queue));
+    }
+
+    public QueueCommand.Get getQueue(Long userId) {
+        UserAccount userAccount = userAccountRepository.findById(userId);
+        Queue queue = queueRepository.findByToken(userAccount.getToken());
+        return QueueCommand.Get.from(queue);
+    }
+
+    @Transactional
+    public void expireQueueByChangingStatus(Long userId) {
+        UserAccount userAccount = userAccountRepository.findById(userId);
+        Queue queue = queueRepository.findByToken(userAccount.getToken());
+        queue.changeStatus(QueueStatus.EXPIRED);
+    }
+
+
+
+    private String generateToken() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid.substring(uuid.lastIndexOf("-") + 1);
+    }
 }
--- src/test/java/com/reservation/ticket/domain/service/QueueServiceTest.java
+++ src/test/java/com/reservation/ticket/domain/service/QueueServiceTest.java
@@ -1,6 +1,11 @@
 package com.reservation.ticket.domain.service;
 
+import com.reservation.ticket.domain.command.QueueCommand;
+import com.reservation.ticket.domain.entity.Queue;
+import com.reservation.ticket.domain.entity.UserAccount;
+import com.reservation.ticket.domain.enums.QueueStatus;
 import com.reservation.ticket.domain.repository.QueueRepository;
+import com.reservation.ticket.domain.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,44 +15,159 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class QueueServiceTest {
 
-    @InjectMocks QueueService queueService;
+    @InjectMocks QueueService sut;
     @Mock QueueRepository queueRepository;
+    @Mock UserAccountRepository userAccountRepository;
 
-    @DisplayName("")
+    @DisplayName("토큰 생성 - 활서화 상태의 사용자가 제한된 사용자(30명)보다 적을때")
     @Test
-    public void given_when_then() {
-        /**
-         *      1. user_id 를 이용한 user 검색
-         *      2. token 생성 : UUID.substring
-         *      3. status :
-         *          1. 기존 대기열에 10개의 `ACTIVE` 가 존재한다면 `WAIT`으로 저장
-         *          2. 기존 대기열에 10개 이하의 `ACTIVE` 가 존재시 `ACTIVE`로 저장
-         *              - 동시성 상태에서 이게 가능한가... 진짜 복합키 써야 하는거 아닐까
-         *                  - 정확히 되는지 동시성 테스트 돌려보고 수정하자
-         *              - 복합키 의 키 (userId, token)
-         *          3. Expired로 상태값을 변경해야 할때
-         *              - 대기열에 있다가 나갈떄 이전의 대기열의 상태값을 `EXPIRED`로 변경 필요
-         *              - expiredAt이 현재 시간과 비교하여 정해진 시간 외의 시간이 됬을때
-         *
-         */
-
-        // 일단 가장 간단한 방법으로 풀어야 할까
-
+    public void givenUserId_whenActiveQueueIsUnder30_thenSaveQueueAsActiveStatus() {
         // given
-        String uuid = UUID.randomUUID().toString();
-        String result = uuid.substring(uuid.lastIndexOf("-") + 1);
+        // userId로 user 검색
+        Long userId = 1L;
+        UserAccount userAccount = UserAccount.of(userId, "Sofia");
+        given(userAccountRepository.findById(userId)).willReturn(userAccount);
 
-        System.out.println("uuid = " + uuid);
-        System.out.println("result = " + result);
+        // 대기열 상태값 - Active(티켓 구매가능) 상태의 대기자가 30명(제한된 인원)보다 적다면
+        // 대기열의 데이터를 Active 로 저장
+        int underCount = 20;
+        given(queueRepository.countByStatus(QueueStatus.ACTIVE)).willReturn(underCount);
+
+        // 대기열 데이터 생성
+        Long queueId = 21L;
+        String token = generateToken();
+
+        Queue queue = Queue.of();
+        queue.saveStatusInQueue(underCount);
+        queue.saveData(userAccount, token);
+        Queue savedQueue = Queue.of(queueId, userAccount, token, QueueStatus.ACTIVE);
+
+        given(queueRepository.save(any(Queue.class))).willReturn(savedQueue);
 
         // when
+        QueueCommand.Get getQueue = sut.createQueue(userId);
 
         // then
+        assertThat(getQueue).isNotNull();
+        assertThat(getQueue.id()).isEqualTo(savedQueue.getId());
+        assertThat(getQueue.status()).isEqualTo(QueueStatus.ACTIVE);
+        assertThat(getQueue.token()).isEqualTo(queue.getToken());
+
+        then(userAccountRepository).should().findById(userId);
+        then(queueRepository).should().countByStatus(QueueStatus.ACTIVE);
+        then(queueRepository).should().save(any(Queue.class));
     }
 
+
+    /**
+     * 토큰 생성 -
+     */
+    @DisplayName("토큰 생성 - 활서화 상태의 사용자가 제한된 사용자보다 만을때")
+    @Test
+    void givenUserId_whenActiveQueueIsOver30_thenSaveQueueAsWaitStatus() {
+        // given
+        // userId로 user 검색
+        Long userId = 1L;
+        UserAccount userAccount = UserAccount.of(userId, "Sofia");
+        given(userAccountRepository.findById(userId)).willReturn(userAccount);
+
+        // 대기열 상태값 - Active(티켓 구매가능) 상태의 대기자가 30명(제한된 인원)보다 많다면
+        // 대기열의 데이터를 Wait 로 저장
+        int overCount = 30;
+        given(queueRepository.countByStatus(QueueStatus.ACTIVE)).willReturn(overCount);
+
+        // 대기열 데이터 생성
+        Long queueId = 21L;
+        String token = generateToken();
+
+        Queue queue = Queue.of();
+        queue.saveStatusInQueue(overCount);
+        queue.saveData(userAccount, token);
+        Queue savedQueue = Queue.of(queueId, userAccount, token, QueueStatus.WAIT);
+
+        given(queueRepository.save(any(Queue.class))).willReturn(savedQueue);
+
+        // when
+        QueueCommand.Get getQueue = sut.createQueue(userId);
+
+        // then
+        assertThat(getQueue).isNotNull();
+        assertThat(getQueue.id()).isEqualTo(savedQueue.getId());
+        assertThat(getQueue.status()).isEqualTo(QueueStatus.WAIT);
+        assertThat(getQueue.token()).isEqualTo(queue.getToken());
+
+        then(userAccountRepository).should().findById(userId);
+        then(queueRepository).should().countByStatus(QueueStatus.ACTIVE);
+        then(queueRepository).should().save(any(Queue.class));
+    }
+
+    /**
+     * 토큰 검색
+     */
+    @Test
+    void givenToken_whenRequestingQueue_thenReturnQueue() {
+        // given
+        Long userId = 1L;
+        UserAccount userAccount = UserAccount.of(userId, "Sofia");
+        given(userAccountRepository.findById(userId)).willReturn(userAccount);
+
+        String token = userAccount.getToken();
+
+        Long queueId = 21L;
+        Queue queue = Queue.of(queueId, userAccount, token, QueueStatus.ACTIVE);
+        given(queueRepository.findByToken(token)).willReturn(queue);
+
+        // when
+        QueueCommand.Get getQueue = sut.getQueue(userId);
+
+        // then
+        assertThat(getQueue).isNotNull();
+        assertThat(getQueue.id()).isEqualTo(queueId);
+        assertThat(getQueue.token()).isEqualTo(token);
+        assertThat(getQueue.status()).isEqualTo(QueueStatus.ACTIVE);
+
+        then(userAccountRepository).should().findById(userId);
+        then(queueRepository).should().findByToken(token);
+    }
+
+    /**
+     * 토큰 만료 - 직접만료 -> 구매가 끝남
+     */
+    @DisplayName("")
+    @Test
+    void givenUserId_whenSearchingQueue_thenExpiredQueueStatus() {
+        // given
+        Long userId = 1L;
+        UserAccount userAccount = UserAccount.of(userId, "Sofia", generateToken());
+        given(userAccountRepository.findById(userId)).willReturn(userAccount);
+
+        Long queueId = 21L;
+        String token = userAccount.getToken();
+        Queue queue = Queue.of(queueId, userAccount, token, QueueStatus.ACTIVE);
+        given(queueRepository.findByToken(token)).willReturn(queue);
+
+        queue.changeStatus(QueueStatus.EXPIRED);
+
+        // when
+        sut.expireQueueByChangingStatus(userId);
+
+        // then
+        assertThat(queue.getStatus()).isEqualTo(QueueStatus.EXPIRED);
+
+        then(userAccountRepository).should().findById(userId);
+        then(queueRepository).should().findByToken(token);
+    }
+
+    private String generateToken() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid.substring(uuid.lastIndexOf("-") + 1);
+    }
 }
