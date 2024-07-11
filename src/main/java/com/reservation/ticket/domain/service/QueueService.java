19a9997a19bac79318e4268e470f5532acb9065a
futuremaker <futuremaker019@gmail.com>
2024-07-12 04:25:35 +0900

    [STEP 8] 비즈니스 구현 및 Usecase 구현

---
+++ src/main/java/com/reservation/ticket/domain/service/QueueService.java
@@ -0,0 +1,121 @@
+package com.reservation.ticket.domain.service;
+
+import com.reservation.ticket.domain.command.QueueCommand;
+import com.reservation.ticket.domain.entity.Queue;
+import com.reservation.ticket.domain.entity.UserAccount;
+import com.reservation.ticket.domain.enums.QueueStatus;
+import com.reservation.ticket.domain.repository.QueueRepository;
+import com.reservation.ticket.domain.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class QueueService {
+
+    private final QueueRepository queueRepository;
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional
+    public QueueCommand.Get createQueue(Long userId) {
+        UserAccount userAccount = userAccountRepository.findById(userId);
+        Queue queue = Queue.of(userAccount, generateToken(), QueueStatus.WAIT);
+        return QueueCommand.Get.from(queueRepository.save(queue));
+    }
+
+    @Transactional(readOnly = true)
+    public List<QueueCommand.Get> selectQueueByStatus(QueueStatus status) {
+        return queueRepository.findAllByQueueStatus(status).stream()
+                .map(QueueCommand.Get::from)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<QueueCommand.Get> selectQueueByStatusPerLimit(QueueStatus status, int limit) {
+        return queueRepository.findAllByQueueStatusPerLimit(status, limit).stream()
+                .map(QueueCommand.Get::from)
+                .toList();
+    }
+
+    public QueueCommand.Get getQueueByUserId(Long userId) {
+        Queue queue = getQueue(userId);
+        return QueueCommand.Get.from(queue);
+    }
+
+    @Transactional
+    public void expireQueueByChangingStatus(Long userId) {
+        Queue queue = getQueue(userId);
+        queue.changeStatus(QueueStatus.EXPIRED);
+    }
+
+    public QueueCommand.Get verifyQueueByUserId(Long userId) {
+        Queue queue = getQueue(userId);
+        // ACTIVE 가 아니면 예외발생
+        queue.verifyQueueStatus();
+        return QueueCommand.Get.from(queue);
+    }
+
+    @Transactional
+    public void renewQueueExpirationDate(String token) {
+        Queue queue = queueRepository.findByToken(token);
+        queue.extendShouldExpiredAt();
+    }
+
+    @Transactional
+    public QueueCommand.Get renewExpirationDate(Long userId) {
+        Queue queue = getQueue(userId);
+        queue.verifyQueueStatus();
+        queue.extendShouldExpiredAt();
+        return QueueCommand.Get.from(queue);
+    }
+
+    private Queue getQueue(Long userId) {
+        UserAccount userAccount = userAccountRepository.findById(userId);
+        return queueRepository.findByToken(userAccount.getToken());
+    }
+
+    /**
+     *  스케줄러 작업
+     *   - ACTIVE 상태인 대기열을 검색하여 만료시간을 현재시간과 비교하여 5분이 초과했다면 EXPIRED 로 상태를 변경
+     *   - 제한된 인원(30명)을 기준으로 현재 ACTIVE 상태인 대기열 목록과 비교하여
+     *      WAIT 상태인 대기열을 ACTIVE 로 변경
+     */
+    @Transactional
+    @Scheduled(cron = "5 * * * * *", zone = "Asia/Seoul")
+    public void changeTokenStatusExpired() {
+        // ACTIVE 상태의 대기열을 EXPIRED 로 변경하는 작업
+        List<Queue> queuesAsActive = queueRepository.findAllByQueueStatus(QueueStatus.ACTIVE);
+        // ACTIVE 상태의 사용자가 한명도 없을때 리스트 확인
+        if (!queuesAsActive.isEmpty()) {
+            queuesAsActive.forEach(queue -> {
+                // 만료시간이 현재시간 기준 5분 초과시 `EXPIRED` 로 변경
+                if (queue.getShouldExpiredAt().plusMinutes(5).isBefore(LocalDateTime.now())) {
+                    queue.changeStatus(QueueStatus.EXPIRED);
+                }
+            });
+        }
+
+        // ACTIVE 상태의 대기열을 EXPIRED 로 변경하는 작업
+        // 30명으로 인원 제한
+        int maxAllowedActive = 30;
+        List<Queue> newQueueAsActive = queueRepository.findAllByQueueStatus(QueueStatus.ACTIVE);
+        if (newQueueAsActive.size() < maxAllowedActive) {
+            // 제한된 인원(30명)과 ACTIVE 대기열 목록을 비교하여 `ACTIVE`로 변경
+            int searchLimit = maxAllowedActive - newQueueAsActive.size();
+            List<Queue> queuesAsWait = queueRepository.findAllByQueueStatusPerLimit(QueueStatus.WAIT, searchLimit);
+            queuesAsWait.forEach(queue -> queue.changeStatus(QueueStatus.ACTIVE));
+        }
+    }
+
+    private String generateToken() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid.substring(uuid.lastIndexOf("-") + 1);
+    }
+}
+
---
+++ src/test/java/com/reservation/ticket/application/usecase/ReservationUsecaseTest.java
@@ -0,0 +1,82 @@
+package com.reservation.ticket.application.usecase;
+
+import com.reservation.ticket.domain.command.ReservationCommand;
+import com.reservation.ticket.domain.command.SeatCommand;
+import com.reservation.ticket.domain.entity.Reservation;
+import com.reservation.ticket.domain.enums.PaymentStatus;
+import com.reservation.ticket.domain.enums.ReservationStatus;
+import com.reservation.ticket.domain.service.ReservationService;
+import com.reservation.ticket.domain.service.SeatService;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * 예약 통합 테스트
+ */
+@SpringBootTest
+class ReservationUsecaseTest {
+
+    @Autowired
+    ReservationUsecase reservationUsecase;
+    @Autowired
+    ReservationService reservationService;
+    @Autowired
+    SeatService seatService;
+
+    @DisplayName("5분동안 결제가 이루어지지 않은 10개의 예약씩 취소하며, 점유한 좌석도 점유 이전의 상태로 된다.")
+    @Test
+    void given_when_then() {
+        // given
+        int cancelledReservationCount = 0;
+        List<Reservation> reservations = reservationService.selectReservationsByReservationStatus(ReservationStatus.CANCELLED);
+        assertThat(reservations.size()).isEqualTo(cancelledReservationCount);
+
+        // when
+        reservationUsecase.releaseOccupiedSeats();
+
+        // then
+        Awaitility.await()
+                .atMost(10, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    int cancelledReservationCountScheduled = 10;
+                    List<Reservation> cancelled = reservationService.selectReservationsByReservationStatus(ReservationStatus.CANCELLED);
+                    assertThat(cancelled).isNotEmpty();
+                    assertThat(cancelled.size()).isEqualTo(cancelledReservationCountScheduled);
+                });
+    }
+
+    @DisplayName("예약정보가 주어지면 예약이 이루어지며, 예약한 좌석이 점유된다.")
+    @Test
+    void givenReservationData_whenMakeReservation_thenNothingReturn() {
+        // given
+        int price = 4000;
+        Long concertScheduleId = 1L;
+        List<Long> seats = List.of(1L, 2L, 5L, 8L);
+        ReservationCommand.Create create = ReservationCommand.Create.of(price, concertScheduleId, seats);
+
+        Long userId = 1L;
+
+        // when
+        ReservationCommand.Get reservation = reservationUsecase.makeReservation(create, userId);
+
+        // then
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.paymentStatus()).isEqualTo(PaymentStatus.NOT_PAID);
+        assertThat(reservation.reservationStatus()).isEqualTo(ReservationStatus.ACTIVE);
+
+        // 저장된 좌석을 찾아 검증한다.
+        List<SeatCommand.Get> findSeats = seatService.selectSeatsByIds(seats);
+        assertThat(findSeats).isNotEmpty();
+        assertThat(findSeats.size()).isEqualTo(seats.size());
+    }
+
+}
