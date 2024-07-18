fac9ef1d07ba83a9eecd61537852f0b928141a03
futuremaker <futuremaker019@gmail.com>
2024-07-18 23:40:23 +0900

    ConcertControllerTest/인터셉터 추가로 인한 QueueService 추가

--- src/test/java/com/reservation/ticket/interfaces/controller/ConcertControllerTest.java
+++ src/test/java/com/reservation/ticket/interfaces/controller/ConcertControllerTest.java
@@ -1,16 +1,28 @@
 package com.reservation.ticket.interfaces.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reservation.ticket.domain.command.ConcertCommand;
+import com.reservation.ticket.domain.service.ConcertService;
+import com.reservation.ticket.domain.service.QueueService;
+import com.reservation.ticket.interfaces.controller.dto.concert.ConcertDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ConcertController.class)
 @ExtendWith(SpringExtension.class)
@@ -19,43 +31,30 @@ class ConcertControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
 
+    @MockBean ConcertService concertService;
+    @MockBean QueueService queueService;
+
     @DisplayName("콘서트 일정 목록을 조회한다.")
     @Test
     void givenNothing_whenRequestingConcertScheduleList_thenReturnsConcertScheduleList() throws Exception {
-//        // given
-//        LocalDateTime localDateTime = LocalDateTime.of(2022, 5, 20, 2, 10);
-//        List<Concert.Select> concerts = List.of(
-//                ConcertCommand.Select.of(1L, 10, localDateTime, (1L, "concert1")),
-//            ConcertScheduleResponse.of(2L, 10,
-//            , ConcertResponse.of(2L, "concert2"))
-//        );
-//
-//        // when
-//        mockMvc.perform(get("/concerts")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(content().bytes(objectMapper.writeValueAsBytes(concerts)));
-//
-//        // then
+        // given
+        List<ConcertCommand.Get> commandSeats = List.of(
+                ConcertCommand.Get.of(1L, "concert1"),
+                ConcertCommand.Get.of(2L, "concert2"),
+                ConcertCommand.Get.of(3L, "concert3")
+        );
+        List<ConcertDto.Response> responses = commandSeats.stream().map(ConcertDto.Response::from).toList();
+        given(concertService.selectAllConcerts()).willReturn(commandSeats);
 
-    }
+        // when
+        mockMvc.perform(get("/api/concerts")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().bytes(objectMapper.writeValueAsBytes(responses)));
 
-    @DisplayName("콘서트 일정 id로 콘서트 일정를 조회한다.")
-    @Test
-    public void givenConcertScheduleId_whenRequestingConcertSchedule_thenReturnsConcertSchedule() throws Exception {
-//        // given
-//        ConcertScheduleResponse concert =
-//                ConcertScheduleResponse.of(1L, 10, LocalDateTime.of(2022, 5, 20, 2, 10), ConcertResponse.of(1L, "concert1"));
-//        Long concertId = 1L;
-//        // when
-//
-//        mockMvc.perform(get("/concerts/%d".formatted(concertId))
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(content().bytes(objectMapper.writeValueAsBytes(concert)));
-//
-//        // then
+        // then
+        then(concertService).should().selectAllConcerts();
     }
 
 }
