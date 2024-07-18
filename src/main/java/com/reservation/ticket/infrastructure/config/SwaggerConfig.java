07ec9b0c5a1a20773591b7d9c9132b41aa455a0a
futuremaker019 <futuremaker019@gmail.com>
2024-07-18 17:19:10 +0900

    Swagger/문자열 토큰을 헤더에 전달할 수 있도록 수정

--- src/main/java/com/reservation/ticket/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/reservation/ticket/infrastructure/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.reservation.ticket.infrastructure.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,8 +15,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes(HttpHeaders.AUTHORIZATION, securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION))
                 .info(apiInfo());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
     }
 
     private Info apiInfo() {
